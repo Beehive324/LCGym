ac35b82eb87a8e676807903650e11e10b103dd78
viery <63168364+Beehive324@users.noreply.github.com>
2025-04-23 15:42:56 +0100

    Time: 0 ms (100.00%) | Memory: 13.3 MB (49.03%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,23 +1,16 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        build up hashmap with index as the key and number as the arget
 
-        go though nums if res = target - nums[i] is in the hashmap
-
-        return i and hashamp.get(res)
-        """
-        compliment_map = {}
+        comp_map = {}
 
         for i in range(len(nums)):
-            compliment_map[nums[i]] = i
+            comp_map[nums[i]] = i
         
-        for i in range(len(nums)-1):
-            res = target - nums[i]
-            if res in compliment_map.keys() and i != compliment_map.get(res): #no 2 indexes are the same
-                return [i, compliment_map.get(res)]
-        
-        
+        for i in range(len(nums)):
+            comp = target - nums[i]
+            if comp in comp_map:
+                if comp_map.get(comp) != i:
+                    return [comp_map.get(comp), i]
 
 
         
