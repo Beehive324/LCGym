21bc3cc4964decadd386b69191fb81754abada3c
viery <63168364+Beehive324@users.noreply.github.com>
2025-04-23 15:15:46 +0100

    Time: 11 ms (84.82%), Space: 13.9 MB (40.48%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        s = re.sub(r'[\W_]', '', s).lower()
+        l = 0
+        r = len(s)-1
+
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+
+
+        
+
+        return True
+        
